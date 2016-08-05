1530d1d4fa35b6dcae65fab8dec968f2bd2adfc2
tocubed <tocubed@gmail.com>
2016-08-05 15:05:53 -0700

    Finalize ConceptNet, add documentation

--- conceptnet.py
+++ conceptnet.py
@@ -1,4 +1,6 @@
 import os.path
+from itertools import takewhile
+from operator import itemgetter
 
 import numpy as np
 import pandas as pd
@@ -6,27 +8,52 @@ import pandas as pd
 RELATIONS_FILEPATH = os.path.join(os.path.dirname(__file__), 'relations')
 CONCEPTS_FILEPATH = os.path.join(os.path.dirname(__file__), 'concepts')
 ASSERTIONS_FILEPATH = os.path.join(os.path.dirname(__file__), 'assertions/')
+DATA_FILENAME = os.path.join(os.path.dirname(__file__), 'data.npz')
 
 
 class Concept(int):
+    """
+    Wraps a ConceptNet array index. Used to lookup and return concepts.
+    """
     def __new__(cls, idx):
         return super(Concept, cls).__new__(cls, idx)
 
 
 class ConceptNet:
+    """
+    Provides access to english-only concepts from ConceptNet5.
+    """
 
     def __init__(self):
+        """
+        Read and initialize relation, concept, and assertion arrays from disk.
+        """
+
         self.relations = pd.read_csv(RELATIONS_FILEPATH, squeeze=True, dtype=np.object, header=None).values
         self.concepts = pd.read_csv(CONCEPTS_FILEPATH, squeeze=True, dtype=np.object, header=None).values
 
         self.assertions = dict()
         for i, relation in enumerate(self.relations):
-            self.assertions[relation] = pd.read_csv(ASSERTIONS_FILEPATH + str(i), delimiter=' ', dtype=np.uint32, header=None).values
+            self.assertions[relation] = pd.read_csv(ASSERTIONS_FILEPATH + str(i),
+                                                    delimiter=' ', dtype=np.uint32, header=None).values
 
             flipped = np.fliplr(self.assertions[relation])
             self.assertions[('!', relation)] = flipped[np.lexsort((flipped[:, 1], flipped[:, 0]))]
 
     def related(self, relation, concept, inverse=False):
+        """
+        Find all concepts related to a concept by a relation.
+
+        :param str relation: the relation as a string, sans the implied '/r/'
+        :param Concept concept: the concept as an index
+        :param bool inverse: whether to use relations in reverse, inward edges vs. outward
+        :rtype: list of Concept
+
+        >>> cn = ConceptNet()
+        >>> related = cn.related('UsedFor', cn.concept('apple'))
+        >>> [cn.string(c) for c in related][:8]
+        ['bait_deer', 'bait_trap', 'compute', 'cook', 'create_music', 'dessert', 'eat', 'enjoy_fruit']
+        """
         if inverse:
             relation = ('!', relation)
 
@@ -35,31 +62,40 @@ class ConceptNet:
 
         result = []
         if idx is not None:
-            # start = idx    #end = (increment idx while ...) # TODO simplify this loop
-            while idx < len(edges) and edges[idx][0] == concept:
-                result.append(edges[idx][1])
-                idx += 1
+            result = list(map(itemgetter(1), takewhile(lambda e: e[0] == concept, edges[idx:])))
         return result
 
-    def concept(self, name):
-        idx = self._binary_search(self.concepts, name)
+    def concept(self, string):
+        """
+        Convert concept from a string to a Concept. Return None if concept does not exist.
+
+        :param str string: the concept as a string, sans the implied '/c/en'
+        :rtype: Concept or None
+
+        >>> cn = ConceptNet()
+        >>> cn.concept('apple')
+        290450
+        """
+        idx = self._binary_search(self.concepts, string)
         return Concept(idx) if idx is not None else None
 
-    def name(self, concept):
+    def string(self, concept):
+        """
+        Convert Concept to a string.
+
+        :param Concept concept: the concept as an index
+        :rtype: str
+
+        >>> cn = ConceptNet()
+        >>> cn.string(Concept(290450))
+        'apple'
+        """
         return self.concepts[concept]
 
     @staticmethod
-    def _binary_search(a, x):
-        i = np.searchsorted(a, x)
-        return i if i != len(a) and a[i] == x else None
-
-
-if __name__ == '__main__':
-    cn = ConceptNet()
-
-    cpt = input()
-    while cpt != 'Q':
-        cpt = input()
-        print(cpt, cn.concept(cpt))
-        for related in cn.related('Antonym', cn.concept(cpt), inverse=True):
-            print("(!, Antonym)", cpt, cn.name(related))
+    def _binary_search(a, value):
+        """
+        Perform binary search and return left-most index, or None if value is not found.
+        """
+        i = np.searchsorted(a, value)
+        return i if i != len(a) and a[i] == value else None
---
+++ extract2.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+
+ASSERTION_FILES = ["data/assertions/part_0" + str(n) + ".csv" for n in range(8)]
+
+
+def extract():
+    """
+    Extract english-only assertions from ConceptNet5, sans metadata, and save them as compressed numpy arrays.
+    """
+    def assertions_filter(df):
+        relations_only = df[1].str.startswith('/r/')
+        english_only = df[2].str.startswith('/c/en/') & df[3].str.startswith('/c/en/')
+
+        concepts_only = (df[2] != '/c/en/') & (df[3] != '/c/en/')
+
+        return relations_only & english_only & concepts_only
+
+    assertions = pd.DataFrame()
+    for file in ASSERTION_FILES:
+        df_iter = pd.read_csv(file, delimiter='\t', usecols=[1, 2, 3], header=None, iterator=True, chunksize=2 ** 16)
+        assertions = pd.concat([assertions] + [chunk[assertions_filter(chunk)] for chunk in df_iter])
+
+    relations = np.sort(assertions[1].unique())
+    concepts = np.unique(np.concatenate((assertions[2].unique(), assertions[3].unique())))
+
+    relations_idx = {relation: idx for idx, relation in enumerate(relations)}
+    concepts_idx = {concept: idx for idx, concept in enumerate(concepts)}
+
+    assertions[1] = assertions[1].apply(lambda r: relations_idx[r])
+    assertions[[2, 3]] = assertions[[2, 3]].applymap(lambda c: concepts_idx[c])
+
+    relation_groups = {str(relation): group[[2, 3]] for relation, group in assertions.groupby(1)}
+    np.savez_compressed('data.npz', relations=relations, concepts=concepts, **relation_groups)
+
+
+extract()
